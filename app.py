ef75008cec5f4d227e924600dece23cfeca0dd70
tarushi2309 <2022csb1135@iitrpr.ac.in>
2025-01-28 17:35:36 +0530

    backend_integration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect, url_for, session,send_file
+from flask import Flask,render_template, request, redirect, url_for, session,send_file,jsonify
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
@@ -45,7 +45,7 @@ def generate_otp(length=6):
 def send_email(sender,receiver):
     otp = generate_otp()
     cursor.execute(
-            'delete from otp_table where user_id = %s and TIMESTAMPDIFF(MINUTE, created_at, NOW()) > 5', (receiver,) )
+            'delete from otp_table where user_id = %s', (receiver,) )
     conn.commit()
     cursor.execute('insert into otp_table (user_id,otp,created_at) values (%s,%s,%s)',(receiver,otp,datetime.now(),))
     conn.commit()
@@ -57,7 +57,7 @@ def send_email(sender,receiver):
     server.sendmail(sender,receiver,text)
 
 @app.route('/')
-def hello2():
+def login():
     return render_template('login.html')
 
 @app.route('/process_login',methods=['GET','POST'])
@@ -79,23 +79,24 @@ def process_otp(email_id):
         otp_given=cursor.fetchone()
         if otp_given:
             if otp_given['otp']==otp:
-                cursor.execute('select * from user where email_id = %s',(email_id,))
+                cursor.execute('select * from user where email_id = %s and not exists(select request_status from user_requests ur where ur.user_id=user.user_id)',(email_id,))
                 global user
                 user=cursor.fetchone()
-                if user['role']=='student':
-                    session['role']=1
-                    session['loggedin']=True
-                    return redirect(url_for("dashboard_student",username=user['username']))
-                elif user['role']=='faculty':
-                    session['role']=2
-                    session['loggedin']=True
-                    return redirect(url_for("dashboard_faculty",username=user['username']))
-                elif user['role']=='admin':
-                    session['role']=3
-                    session['loggedin']=True
-                    return redirect(url_for("dashboard_admin",username=user['username']))
+                if user:
+                    if user['role']=='student':
+                        session['role']=1
+                        session['loggedin']=True
+                        return redirect(url_for("dashboard_student",username=user['username']))
+                    elif user['role']=='faculty':
+                        session['role']=2
+                        session['loggedin']=True
+                        return redirect(url_for("dashboard_faculty",username=user['username']))
+                    elif user['role']=='admin':
+                        session['role']=3
+                        session['loggedin']=True
+                        return redirect(url_for("dashboard_admin",username=user['username']))
                 else:
-                    msg="Incorrect Email Id !! Please re enter correct email id"
+                    msg="Email_id user does not exist!! Please re enter correct email id"
                     return render_template("login.html",msg=msg)
             else:
                 msg="Incorrect OTP !! Please try again"
@@ -130,12 +131,13 @@ def dashboard_faculty(username):
         faculty = cursor.fetchone()
         cursor.execute('SELECT * FROM course where faculty_id = %s',(faculty['faculty_id'],))  
         courses = cursor.fetchall()
+        print(f"courses : {courses}")
         return render_template("faculty_dashboard.html",user=user,faculty=faculty,courses=courses)
     else:
         return "You are not authorised to view this page!!"
 
-@app.route('/dashboard_admin/<username>')
-def dashboard_admin(username):
+@app.route('/dashboard_admin')
+def dashboard_admin():
     if session['role']==3:
         cursor.execute('SELECT * FROM admin where user_id = %s',(user['user_id'],))  
         admin = cursor.fetchone()
@@ -154,23 +156,89 @@ def create_course(faculty_id):
             code=request.form['coursecode']
             year=request.form['year']
             dep=request.form['department']
-            print(f"fACULTY ID : {faculty_id}")
             cursor.execute('INSERT INTO course(course_name,faculty_id,course_code,LTPC,sem,course_status,credits) VALUES (%s, %s, %s, %s, %s,%s,%s)', (course_name, faculty_id, code, ltpc, sem,'pending_admin_approval',ltpc[-1],))
             conn.commit()
-            return render_template('coursefloat.html',faculty_id=faculty_id,msg='Course created successfully. Students shall be able to enroll after admin approval!!')
-        return render_template('coursefloat.html',faculty_id=faculty_id,msg='')
+            return render_template('coursefloat.html',username=user['username'],faculty_id=faculty_id,msg='Course created successfully. Students shall be able to enroll after admin approval!!')
+        return render_template('coursefloat.html',username=user['username'],faculty_id=faculty_id,msg='')
     else:
         return "You are not authorised to view this page!!"
 
-@app.route('/pending_approvals/<faculty_id>')
+@app.route('/pending_approvals/<faculty_id>',methods=['GET','POST'])
 def pending_approvals(faculty_id):
     if session['role']==2:
-        cursor.execute(
-            'SELECT u.username , s.entry_no, c.course_name FROM course c JOIN student_course sc ON sc.course_id = c.course_id JOIN student s ON sc.student_id = s.student_id JOIN user u ON s.user_id = u.user_id WHERE c.faculty_id = %s AND sc.status = %s', (faculty_id, 'pending_instructor_approval'))
-        students = cursor.fetchall()
-        return render_template('faculty_approval.html',students=students)
+        if request.method=='POST':
+            student_id = request.json.get('student_id')
+            course_id = request.json.get('course_id')
+            action = request.json.get('action')
+            curr_status=request.json.get('status')
+            print(f'{student_id} {course_id} {action} {curr_status}')
+            new_status=""
+
+            if action == "approve":
+                if curr_status == 'pending instructor approval':
+                    new_status = "pending advisor approval"
+                    cursor.execute('select dep from student where student_id = %s',(student_id,))
+                    dep=cursor.fetchone()
+                    cursor.execute('select faculty_id from faculty_advisor where dep = %s',(dep['dep'],))
+                    advisor=cursor.fetchone()
+                    cursor.execute('update approvals set faculty_id =%s where student_id = %s and course_id=%s',(advisor['faculty_id'],student_id,course_id))
+                    conn.commit()
+                else:
+                    new_status='enrolled'
+                    cursor.execute('delete from approvals where student_id=%s and course_id=%s',(student_id,course_id))
+                    conn.commit()
+            elif action == "reject":
+                new_status = "rejected"
+                cursor.execute('delete from approvals where student_id=%s and course_id=%s',(student_id,course_id))
+                conn.commit()
+            cursor.execute(
+            'UPDATE student_course SET status = %s WHERE student_id = %s AND course_id = %s',(new_status, student_id, course_id))
+            conn.commit()
+            cursor.execute('SELECT u.username,s.entry_no,stu_course.* from (select sc.student_id,sc.status,c.course_name,c.course_id FROM course c JOIN student_course sc on sc.course_id=c.course_id where c.faculty_id = %s and sc.status = %s) stu_course JOIN student s on s.student_id=stu_course.student_id JOIN user u ON s.user_id = u.user_id',(faculty_id,'pending instructor approval',))
+            students = cursor.fetchall()
+            cursor.execute('SELECT s.student_id,u.username,s.entry_no,sc.status,c.course_name,c.course_id from student s JOIN user u on s.user_id = u.user_id JOIN student_course sc on sc.student_id=s.student_id JOIN course c on c.course_id = sc.course_id  where s.dep in (select dep from faculty where faculty_id = %s and faculty_advisor = %s) and sc.status = %s',(faculty_id,1,'pending advisor approval'))
+            students.append(cursor.fetchall())
+            return jsonify({"success": True, "students": students,"faculty_id":faculty_id,"username":user['username']})
+        else:
+            cursor.execute('SELECT u.username,s.entry_no,stu_course.* from (select sc.student_id,sc.status,c.course_name,c.course_id FROM course c JOIN student_course sc on sc.course_id=c.course_id where c.faculty_id = %s and sc.status = %s) stu_course JOIN student s on s.student_id=stu_course.student_id JOIN user u ON s.user_id = u.user_id',(faculty_id,'pending instructor approval',))
+            students = cursor.fetchall()
+            cursor.execute('SELECT s.student_id,u.username,s.entry_no,sc.status,c.course_name,c.course_id from student s JOIN user u on s.user_id = u.user_id JOIN student_course sc on sc.student_id=s.student_id JOIN course c on c.course_id = sc.course_id  where s.dep in (select dep from faculty where faculty_id = %s and faculty_advisor = %s) and sc.status = %s',(faculty_id,1,'pending advisor approval',))
+            students.append(cursor.fetchall())
+            print(f'sending students{students}')
+            return render_template('faculty_approval.html',students=students,faculty_id=faculty_id,username=user['username'])
     else:
        return "You are not authorised to view this page!!" 
+
+@app.route('/courses_available/<username>',methods=['GET','POST'])
+def courses_available(username):
+    if session['role']==1:
+        cursor.execute('SELECT * FROM student where user_id = %s',(user['user_id'],))  
+        student = cursor.fetchone()
+        if request.method=='POST':
+            course_id = request.json.get('course_id')
+            action = request.json.get('action')
+            faculty_id = request.json.get('faculty_id')
+            cursor.execute('select status from student_course where stduent_id =%s and course_id =%s',(student['student_id'],course_id,) )
+            curr_status=cursor.fetchone()
+            if action == 'credit':
+                if curr_status['status'] and curr_status['status']!='dropped':
+                    return render_template("courses_available.html",user=user,student=student,courses=courses,msg='Already credited')
+                cursor.execute('update course set no_of_enrollments = no_of_enrollments+1 where course_id = %s',(course_id,))
+                cursor.execute('insert into student_course(student_id,course_id,status,grade) values(%s,%s,%s,%s)',(student['student_id'],course_id,'pending instructor approval','NA',))
+                conn.commit()
+            elif action == 'audit':
+                cursor.execute('update student_course set status = %s where student_id=%s and course_id = %s',('audit',student['student_id'],course_id,))
+                conn.commit()
+            else:
+                if not curr_status['status']:
+                    return render_template("courses_available.html",user=user,student=student,courses=courses,msg='Cannot drop course not enrolled')
+                cursor.execute('update student_course set status = %s where student_id=%s and course_id = %s',('dropped',student['student_id'],course_id,))
+                conn.commit()
+        cursor.execute('SELECT * from course c join faculty f on f.faculty_id=c.faculty_id join user u on u.user_id=f.user_id where c.admin_approval_status = %s',('approved',))  
+        courses = cursor.fetchall()
+        return render_template("courses_available.html",user=user,student=student,courses=courses)
+    else:
+        return "You are not authorised to view this page!!"
 
 @app.route('/signup',methods=['GET','POST'])
 def signup_process():
@@ -189,17 +257,27 @@ def signup_process():
             degree = request.form['degree']
             department = request.form['department']
             year_of_entry=request.form['year_of_entry']
+            cursor.execute('insert into user(username,email_id,role) values(%s,%s,%s)',(name,email_id,'student'))
+            cursor.execute('select user_id from user where email_id = %s',(email_id,))
+            user_id=cursor.fetchone()
+            cursor.execute('insert into user_requests(user_id) values %s',(user_id['user_id'],))
             cursor.execute(
-            'INSERT INTO student(student_name,entry_no,email_id,degree,department,year_of_entry) VALUES(%s,%s, %s,%s, %s,%s)', (name,entry_no,email_id,degree,department,year_of_entry,) )
+            'INSERT INTO student(user_id,entry_no,degree,dep,year_of_entry) VALUES(%s,%s,%s, %s,%s)', (user_id['user_id'],entry_no,degree,department,year_of_entry,) )
             conn.commit()
-            return render_template("login.html",msg="Signup Successful. You may login Now")
+            print('commited')
+            return render_template("login.html",msg="Signup Successful. You will be able to login after admin approval")
         elif user_type =="faculty":
             name = request.form['name']
             email_id = request.form['email']
+            dep=request.form['department']
+            cursor.execute('insert into user(username,email_id,role) values(%s,%s,%s)',(name,email_id,'faculty'))
+            cursor.execute('select user_id from user where email_id = %s',(email_id,))
+            user_id=cursor.fetchone()
+            cursor.execute('insert into user_requests(user_id) values %s',(user_id['user_id'],))
             cursor.execute(
-            'INSERT INTO faculty(faculty_name,email_id) VALUES(%s, %s)', (name,email_id,) )
+            'INSERT INTO faculty(user_id,dep) VALUES(%s, %s)', (user_id['user_id'],dep,) )
             conn.commit()
-            return render_template("login.html",msg="Signup Successful. You may login Now")
+            return render_template("login.html",msg="Signup Successful.You will be able to login after admin approval ")
 
 
 @app.route('/user_category',methods=['GET','POST'])
@@ -214,6 +292,68 @@ def user_category():
         return render_template("signup_student.html")
     elif(user_type=='faculty'):
         return render_template("signup_faculty.html")
+
+@app.route('/admin/approve_user', methods=['GET','POST'])
+def approve_user():
+    if session['role'] != 3:
+        return "You are not authorized to perform this action!", 403
+    if request.method == 'POST':
+        request_id = request.json.get('request_id')
+        action = request.json.get('action')  
+
+        if action == 'approve':
+            cursor.execute('delete from user_requests WHERE request_id = %s', (request_id,))
+        elif action == 'reject':
+            cursor.execute('UPDATE user_requests SET request_status = %s WHERE request_id = %s', ('rejected', request_id))
+            cursor.execute('select user_id from user_requests where request_id = %s',('rejected',))
+            user_id=cursor.fetchone()
+            cursor.execute('delete from user where user_id=%s',(user['user_id']))
+
+        conn.commit()
+        cursor.execute('select * from user_requests ur JOIN user u on ur.user_id=u.user_id')
+        pending_users=cursor.fetchall()
+        return jsonify({success:True,"user":pending_users})
+    cursor.execute('select * from user_requests ur JOIN user u on ur.user_id=u.user_id')
+    pending_users=cursor.fetchall()
+    return render_template('admin_enrollments.html',user=pending_users)
+
+@app.route('/admin/approve_course', methods=['GET','POST'])
+def approve_course():
+    if session['role'] != 3:
+        return "You are not authorized to perform this action!", 403
+    if request.method == 'POST':
+        course_id = request.json.get('course_id')
+        action = request.json.get('action')  # 'approve' or 'reject'
+
+        if action == 'approve':
+            cursor.execute('UPDATE course SET admin_approval_status = %s WHERE course_id = %s', ('approved', course_id))
+        elif action == 'reject':
+            cursor.execute('delete from course WHERE course_id = %s', (course_id))
+        conn.commit()
+        cursor.execute('select * from course where admin_approval_status=%s',('pending'))
+        pending_course=cursor.fetchall()
+        return jsonify({"success": True,"courses":pending_course})
+    else:
+        cursor.execute('select c.*,u.username from course c join faculty f on f.faculty_id = c.faculty_id join user u on u.user_id=f.user_id where admin_approval_status=%s',('pending'))
+        pending_course=cursor.fetchall()
+        return render_template('admin_courses.html',courses=pending_course)
+
+@app.route("/course/<course_id>/<username>")
+def course(course_id,username):
+    if session['role']==1:
+        cursor.execute('select c.*,u.username,f.dep from course c join faculty f on f.faculty_id = c.faculty_id join user u on u.user_id=f.user_id where course_id = %s',(course_id))
+        course=cursor.fetchone()
+        cusor.execute('select s.*,sc.status from student_course sc join student s on sc.student_is=s.student_id where sc.course_id = %s',(course_id))
+        students=cursor.fetchall()
+        return render_template('course_details_student.html',course=course,students=students,username=username)
+    elif session['role']==2:
+        cursor.execute('select c.*,u.username,f.dep,f.faculty_id from course c join faculty f on f.faculty_id = c.faculty_id join user u on u.user_id=f.user_id where course_id = %s',(course_id))
+        course=cursor.fetchone()
+        cusor.execute('select s.*,sc.status from student_course sc join student s on sc.student_is=s.student_id where sc.course_id = %s',(course_id))
+        students=cursor.fetchall()
+        return render_template('course_details_faculty.html',course=course,students=students,username=username)
+    else:
+        return "You are not authorised to view this page!!"
 
 """@app.route('/profile_mentee/<username>',methods=['GET','POST'])
 def profile_mentee(username):
@@ -856,7 +996,7 @@ def submit_feedback():
         mysql.connection.commit()
         return redirect(url_for('dashboard_mentee',username=username))
 
-    return render_template('feedback_form.html',username=username)
+    return render_template('feedback_form.html',username=username)"""
 
 
 @app.route('/logout')
@@ -867,11 +1007,12 @@ def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-    return redirect(url_for('hello'))"""
+    session.pop('role',None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    with app.app_context():
-        init_db()
+    #with app.app_context():
+        #init_db()
     app.run(debug=True)
             
  
--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE student (
     dep ENUM('COMPUTER SCIENCE AND ENGINEERING','ELECTRICAL ENGINEERING','MECHANICAL ENGINEERING','MATHS AND COMPUTING','CHEMICAL ENGINEERING','METALLURGY ENGINEERING','CIVIL ENGINEERING'),
     entry_no varchar(11),
     year_of_entry int,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE otp_table(
@@ -28,39 +28,38 @@ CREATE TABLE otp_table(
 CREATE TABLE faculty(
     faculty_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     user_id int,
+    faculty_advisor int default 0,
     dep ENUM('COMPUTER SCIENCE AND ENGINEERING','ELECTRICAL ENGINEERING','MECHANICAL ENGINEERING','MATHS AND COMPUTING','CHEMICAL ENGINEERING','METALLURGY ENGINEERING','CIVIL ENGINEERING'),
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE course (
     course_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     course_name VARCHAR(100),
     faculty_id int,
+    admin_approval_status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
     course_status enum('running','completed','pending_admin_approval'),
     no_of_enrollments int default 0,
     LTPC varchar(8),
+    sem varchar(8),
     credits int,
-    sem varchar(10),
     course_code varchar(100),
     FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_id)
 );
 
 CREATE TABLE student_course (
-    student_id int,
-    course_id int ,
-    status enum('audit','enrolled','dropped','pending instructor approval', 'pending advisor approval','completed'),
+    student_id int not null,
+    course_id int not null ,
+    primary key(student_id,course_id),
+    status enum('audit','enrolled','dropped','pending instructor approval', 'pending advisor approval','completed','rejected'),
     grade varchar(2),
     FOREIGN KEY (student_id) REFERENCES student(student_id),
     FOREIGN KEY (course_id) REFERENCES course(course_id)
 );
 
-CREATE TABLE faculty_advisor(
-    faculty_id int primary key,
-    year_of_entry int,
-    degree ENUM('BTECH','MTECH','PHD','MSC'),
-    dep ENUM('COMPUTER SCIENCE AND ENGINEERING','ELECTRICAL ENGINEERING','MECHANICAL ENGINEERING','MATHS AND COMPUTING','CHEMICAL ENGINEERING','METALLURGY ENGINEERING'),
-    FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_id)
-);
+
+create index stat on student_course(status);
+
 
 CREATE TABLE admin(
     admin_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -68,11 +67,23 @@ CREATE TABLE admin(
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
+CREATE TABLE user_requests (
+    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    request_status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
+);
+
 INSERT into user(username,email_id,role) values('TARUSHI','2022csb1135@iitrpr.ac.in','student');
-INSERT into user(username,email_id,role) values('Dr. Puneet Goyal','puneet@iitrpr.ac.in','faculty');
+INSERT into user(username,email_id,role) values('Dr. Puneet Goyal','tarushi.tanejag1112@gmail.com','faculty');
+
 Insert into student(user_id,degree,dep,entry_no,year_of_entry) values(1,'BTECH','COMPUTER SCIENCE AND ENGINEERING','2022csb1135',2022);
 INSERT into faculty(user_id,dep) values(2,'COMPUTER SCIENCE AND ENGINEERING');
-INSERT into course(course_name,faculty_id,course_status,LTPC,credits,course_code,sem) values('Development Engineering Project',1,'running','3-5-5-3',3,'CP301','2024-II');
-INSERT into student_course(student_id,course_id,status,grade) values(1,1,'enrolled','NA');
-insert into user(username,email_id,role) values('tarushi','tarushi.tanejag1112@gmail.com','faculty');
-insert into faculty(user_id,dep) values(3,'ELECTRICAL ENGINEERING');
+
+INSERT into course(course_name,faculty_id,course_status,LTPC,credits,course_code,sem,admin_approval_status) values('Development Engineering Project',1,'running','3-5-5-3',3,'CP301','2024-II','approved');
+INSERT into student_course(student_id,course_id,status,grade) values(1,1,'pending instructor approval','NA');
+insert into user(username,email_id,role) values('Apurva Mudgal','tanejatarushi23@gmail.com','faculty');
+
+INSERT into faculty(user_id,dep,faculty_advisor) values(3,'COMPUTER SCIENCE AND ENGINEERING',1);
+
+INSERT into course(course_name,faculty_id,course_status,LTPC,credits,course_code,sem,admin_approval_status) values('BTP',1,'running','3-5-5-3',3,'CP301','2024-II','approved');
